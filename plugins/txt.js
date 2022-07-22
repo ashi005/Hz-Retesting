34f27f7df50d92e8c186d147f1909e0c4674b339
ashi005 <109684745+ashi005@users.noreply.github.com>
2022-07-22 15:35:57 +0530

    Update txt.js

--- plugins/txt.js
+++ plugins/txt.js
@@ -2,7 +2,7 @@ const evt = require('../events');
 
 
 
-  evt.getCMD({pattern: 'start$', fromMe: true, deleteCommand: false, desc: "ping pong 📨"}, (async (message, match) => {
+  evt.addCommand({pattern: 'start$', fromMe: true, deleteCommand: false, desc: "ping pong 📨"}, (async (message, match) => {
 
  await message.client.sendMessage(message.jid, { text: 'hi hello' }) 
 
