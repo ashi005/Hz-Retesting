2ae9d2d1ae8194d58c8e799ccc436cd56366b5a1
ashi005 <109684745+ashi005@users.noreply.github.com>
2022-07-22 13:12:04 +0530

    Update _plugins.js

--- plugins/_plugins.js
+++ plugins/_plugins.js
@@ -55,7 +55,7 @@ Neotro.addCommand({pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DE
         try {
             require('./' + plugin_name);
         } catch (e) {
-            fs.unlinkSync('/root/ales/plugins/' + plugin_name + '.js')
+            fs.unlinkSync('/root/queendianamd/plugins/' + plugin_name + '.js')
             return await message.sendMessage(Lang.INVALID_PLUGIN + ' ```' + e + '```');
         }
 
