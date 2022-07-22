ada10c184e37319f630a88989e8bc652b3f5fdb0
ashi005 <109684745+ashi005@users.noreply.github.com>
2022-07-22 13:10:23 +0530

    Update _plugins.js

--- plugins/_plugins.js
+++ plugins/_plugins.js
@@ -6,7 +6,8 @@ NEOTROX - TEENUHX
 
 const Neotro = require('../events');
 const Heroku = require('heroku-client');
-const Config = require('../cosec');
+const Config = require('../config');
+const cosec = require('../cosec');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 const fs = require('fs');
@@ -19,11 +20,11 @@ let msg = Config.LANG == 'SI' || Config.LANG == 'AZ' ? '*අනුමත කර�
 let inmsg = Config.LANG == 'SI' || Config.LANG == 'AZ' ? '*අනුමත නොකරණ ලද ප්ලගීනකි..!* ❌' : '*Not Approved Plugin.* ❌'
 
 const heroku = new Heroku({
-    token: Config.HEROKU.API_KEY
+    token: cosec.HEROKU.API_KEY
 });
 
 
-let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+let baseURI = '/apps/' + cosec.HEROKU.APP_NAME;
 
 Neotro.addCommand({pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, warn: Lang.WARN}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.NEED_URL + '.install https://gist.github.com/Neotro23/4232b1c8c4734e1f06c3d991149c6fbd')
@@ -67,43 +68,3 @@ Neotro.addCommand({pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DE
     }
 }));
 
-Neotro.addCommand({pattern: 'pluglist', fromMe: true, desc: Lang.PLUGIN_DESC }, (async (message, match) => {
-    var mesaj = Lang.PLIST;
-    var plugins = await Db.PluginDB.findAll();
-    if (plugins.length < 1) {
-        return await message.sendMessage(Lang.NO_PLUGIN);
-    } else {
-        plugins.map(
-            (plugin) => {
-                let vf = plugin.dataValues.url.includes('Neotro23') ? msg : inmsg
-                mesaj += '```' + plugin.dataValues.name + '```: ' + '🧚‍♂️ \n' + vf + '\n\n';
-            }
-        );
-        return await message.client.sendMessage(message.jid, mesaj, MessageType.text);
-    }
-}));
-
-Neotro.addCommand({pattern: 'remove(?: |$)(.*)', fromMe: true, desc: Lang.REMOVE_DESC}, (async (message, match) => {
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_PLUGIN);
-    if (!match[1].startsWith('.')) match[1] = '.' + match[1];
-    var plugin = await Db.PluginDB.findAll({ where: {name: match[1]} });
-    if (plugin.length < 1) {
-        return await message.sendMessage(message.jid, Lang.NOT_FOUND_PLUGIN, MessageType.text);
-    } else {
-        await plugin[0].destroy();
-        delete require.cache[require.resolve('./' + match[1] + '.js')]
-        fs.unlinkSync('./plugins/' + match[1] + '.js');
-        await message.client.sendMessage(message.jid, Lang.DELETED, MessageType.text);
-        
-        await new Promise(r => setTimeout(r, 1000));
-    
-        await message.sendMessage(NLang.AFTER_UPDATE);
-
-        console.log(baseURI);
-        await heroku.delete(baseURI + '/dynos').catch(async (error) => {
-            await message.sendMessage(error.message);
-
-        });
-    }
-
-}));
